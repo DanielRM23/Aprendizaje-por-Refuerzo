166854632b2ed80ff404f7dfbdbc2258a4f2b41d
DanielRM23 <DanielRM@ciencias.unam.mx>
2025-02-15 12:02:49 -0600

    Agrego creación de tablero, espacios vacíos y elegir posición aleatoria

--- Tarea1/Programas/TicTacToe.py
+++ Tarea1/Programas/TicTacToe.py
@@ -13,11 +13,26 @@ class TicTacToe:
 
     def crear_tablero(self, tamanio):
         tablero = np.zeros((tamanio, tamanio), dtype=int)  
-        print(tablero)
+        return tablero
 
+    # Check for empty places on tablero
+    def posibilidades(self, tablero):
+        l = []
+        for i in range(len(tablero)):
+            for j in range(len(tablero)):
+                if tablero[i][j] == 0:
+                    l.append((i, j))
+        return l
 
+# Select a random place for the jugador
+    def random_place(self, tablero, jugador):
+        selection = self.posibilidades(tablero)
+        posicion = random.choice(selection)
+        tablero[posicion] = jugador
+        return(tablero)
 
 game = TicTacToe()
+tablero = game.crear_tablero(3)
 
-game.crear_tablero(3)
-
+print(game.posibilidades(tablero))
+print(game.random_place(tablero, 1))
