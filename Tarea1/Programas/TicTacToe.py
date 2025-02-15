651a5e38d2e1b9a9a271c1da622e7902b4b4ff66
DanielRM23 <DanielRM@ciencias.unam.mx>
2025-02-15 12:32:53 -0600

    Agrego métodos para revisar si gana por columnas, filas o diagonales

--- Tarea1/Programas/TicTacToe.py
+++ Tarea1/Programas/TicTacToe.py
@@ -31,6 +31,56 @@ class TicTacToe:
         tablero[posicion] = jugador
         return(tablero)
 
+    # Checks whether the jugador has three
+    # of their marks in a horizontal row 
+    def row_win(self, tablero, jugador):
+        for x in range(len(self.tablero)):
+            win = True
+            for y in range(len(self.tablero)):
+                if self.tablero[x][y] != self.jugador:
+                    win = False
+                    continue
+    
+            if win == True:
+                return(win)
+        return(win)
+
+# Checks whether the jugador has three
+    # of their marks in a horizontal row 
+    def col_win(self, tablero, jugador):
+        for x in range(len(self.tablero)):
+            win = True
+            for y in range(len(self.tablero)):
+                if self.tablero[y][x] != self.jugador:
+                    win = False
+                    continue
+    
+            if win == True:
+                return(win)
+        return(win)
+
+    # Checks whether the jugador has three
+    # of their marks in a diagonal row
+    def diag_win(self, tablero, jugador):
+        # Verificar la diagonal principal
+        win = True
+        for x in range(len(tablero)):
+            if tablero[x, x] != jugador:
+                win = False
+                break
+        if win:
+            return True
+        
+        # Verificar la diagonal secundaria
+        win = True
+        for x in range(len(tablero)):
+            y = len(tablero) - 1 - x
+            if tablero[x][y] != jugador:
+                win = False
+                break
+        return win
+
+
 game = TicTacToe()
 tablero = game.crear_tablero(3)
 
