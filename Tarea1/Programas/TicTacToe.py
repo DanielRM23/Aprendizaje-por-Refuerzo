7528f89bcdbf53d9e05eaaa7d84c129c128c74f4
DanielRM23 <DanielRM@ciencias.unam.mx>
2025-02-17 10:18:29 -0600

    Se agrega una mejor versión para la creación del tablero

--- Tarea1/Programas/TicTacToe.py
+++ Tarea1/Programas/TicTacToe.py
@@ -1,121 +1,89 @@
-import numpy as np 
-import random 
-from time import sleep
+import numpy as np
+import random
 
+class TicTacToe:
+    def __init__(self, tamanio=3):
+        self.tamanio = tamanio #Tamaño del tablero 
+        self.tablero = self.crear_tablero() #Se crea el tablero 
+        self.estados_valores = {}  # Diccionario para almacenar los valores de los estados
+        self.politica = {}  # Diccionario para almacenar la política óptima
+    
+    def crear_tablero(self):
+        # Se crea un tablero con dimensiones taminio x tamanio
+        # El tablero es una matriz de ceros de dicho tamaño, donde cada cero representa una posición vacía en el tablero
+        return np.zeros((self.tamanio, self.tamanio), dtype=int) 
 
-#Define los estados SS como todas las configuraciones válidas del tablero.
+    def obtener_estado(self, tablero):
+        # Esto se hace con el fin utilizar el estado del tablero como clave en el diccionario de valores de estado
+        return tuple(map(tuple, tablero))
 
-class TicTacToe: 
-
-    def crear_tablero(self, tamanio):
-        tablero = np.zeros((tamanio, tamanio), dtype=int)  
-        return tablero
-
-    # Check for empty places on tablero
     def posibilidades(self, tablero):
-        l = []
-        for i in range(len(tablero)):
-            for j in range(len(tablero)):
+        # Devuelve una lista de todas las posiciones vacías en el tablero
+        # Esto se traduce a verificar que en la posición (i,j) del tablero haya un 0 (cero)
+        posiciones = []
+        for i in range(self.tamanio): 
+            for j in range(self.tamanio):
                 if tablero[i][j] == 0:
-                    l.append((i, j))
-        return l
+                    posiciones.append((i,j))
 
-# Select a random place for the jugador
-    def random_place(self, tablero, jugador):
-        selection = self.posibilidades(tablero)
-        posicion = random.choice(selection)
-        tablero[posicion] = jugador
-        return(tablero)
+        return posiciones
 
-    # Checks whether the jugador has three
-    # of their marks in a horizontal row 
+    def aplicar_accion(self, tablero, accion, jugador):
+        # Crea una copia del tablero original para no modificar el tablero existente directamente.
+        # Luego, actualiza la posición especificada por la acción con el número del jugador.
+        # Finalmente, devuelve el nuevo tablero con la acción aplicada.
+        nuevo_tablero = tablero.copy()
+        nuevo_tablero[accion] = jugador
+        return nuevo_tablero
+
+
     def row_win(self, tablero, jugador):
-        for x in range(len(self.tablero)):
-            win = True
-            for y in range(len(self.tablero)):
-                if self.tablero[x][y] != self.jugador:
-                    win = False
-                    continue
+        for x in range(len(tablero)):  # Recorremos cada fila
+            win = True  # Inicializamos la variable win como True
+            for y in range(len(tablero[0])):  # Recorremos cada columna en la fila
+                if tablero[x][y] != jugador:  # Si encontramos un valor diferente al del jugador
+                    win = False  # Cambiamos win a False
+                    break  # Salimos del ciclo
+            if win:  # Si no se encontró un valor diferente, significa que el jugador ha ganado en esta fila
+                return True
+        return False  # Si no hay victoria en ninguna fila, devolvemos False
     
-            if win == True:
-                return(win)
-        return(win)
-
-# Checks whether the jugador has three
-    # of their marks in a horizontal row 
     def col_win(self, tablero, jugador):
-        for x in range(len(self.tablero)):
-            win = True
-            for y in range(len(self.tablero)):
-                if self.tablero[y][x] != self.jugador:
-                    win = False
-                    continue
-    
-            if win == True:
-                return(win)
-        return(win)
+        for x in range(len(tablero[0])):  # Recorremos cada columna
+            win = True  # Inicializamos la variable win como True
+            for y in range(len(tablero)):  # Recorremos cada fila en la columna
+                if tablero[y][x] != jugador:  # Si encontramos un valor diferente al del jugador
+                    win = False  # Cambiamos win a False
+                    break  # Salimos del ciclo
+            if win:  # Si no se encontró un valor diferente, significa que el jugador ha ganado en esta columna
+                return True
+        return False  # Si no hay victoria en ninguna columna, devolvemos False
 
-    # Checks whether the jugador has three
-    # of their marks in a diagonal row
     def diag_win(self, tablero, jugador):
         # Verificar la diagonal principal
         win = True
         for x in range(len(tablero)):
-            if tablero[x, x] != jugador:
-                win = False
-                break
-        if win:
+            if tablero[x][x] != jugador:  # Si encontramos un valor diferente en la diagonal principal
+                win = False  # Cambiamos win a False
+                break  # Salimos del ciclo
+        if win:  # Si no se encontró un valor diferente, significa que el jugador ha ganado en la diagonal principal
             return True
-        
+
         # Verificar la diagonal secundaria
         win = True
         for x in range(len(tablero)):
-            y = len(tablero) - 1 - x
-            if tablero[x][y] != jugador:
-                win = False
-                break
-        return win
+            y = len(tablero) - 1 - x  # Calculamos el índice de la columna de la diagonal secundaria
+            if tablero[x][y] != jugador:  # Si encontramos un valor diferente en la diagonal secundaria
+                win = False  # Cambiamos win a False
+                break  # Salimos del ciclo
+        return win  # Retornamos si el jugador ha ganado en la diagonal secundaria
 
-    # Evaluates whether there is
-    # a winner or a tie
-    
-    
-    def evaluate(self, tablero):
-        winner = 0
+    def es_terminal(self, tablero):
+        for jugador in [1, 2]:
+            if self.row_win(tablero, jugador) or self.col_win(tablero, jugador) or self.diag_win(tablero, jugador):
+                return jugador
 
-        for self.jugador in [1, 2]:
-            if (self.row_win(self.tablero, self.jugador) or
-                    self.col_win(self.tablero, self.jugador) or
-                    self.diag_win(self.tablero, self.jugador)):
-
-                winner = self.jugador
-
-        if np.all(tablero != 0) and winner == 0:
-            winner = -1
-        return winner
-
-    def play_game(self):
-        self.tablero, winner, counter = self.crear_tablero(3), 0, 1
-        print("Tablero inicial")
-        print(self.tablero)
-        sleep(2)
-    
-        while winner == 0:
-            for self.jugador in [1, 2]:
-                self.tablero = self.random_place(self.tablero, self.jugador)
-                print("\n Tablero después de " + str(counter) + " movimiento")
-                print(self.tablero)
-                sleep(2)
-                counter += 1
-                winner = self.evaluate(self.tablero)
-                if winner != 0:
-                    break
-        return(winner)
-
-game = TicTacToe()
-tablero = game.crear_tablero(3)
-
-# print(game.posibilidades(tablero))
-# print(game.random_place(tablero, 1))
-
-print("El ganador es: " + str(game.play_game()))
+        if not self.posibilidades(tablero):
+            return -1  # No hay más movimientos, el juego terminó en empate
+        else:
+            return 0  # El juego puede continuar, hay movimientos posibles
